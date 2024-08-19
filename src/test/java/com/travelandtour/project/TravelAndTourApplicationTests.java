a8f0090fb113a57daed8f877c180ae3a9cadb30d
Aung Thein Kyaw <aungtheinkyaw.atk04@gmail.com>
2024-08-19 19:55:19 +0630

    project setup

---
+++ src/test/java/com/travelandtour/project/TravelAndTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelandtour.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAndTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
